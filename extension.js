bac63ccd1a09b33ebe7c29663df7058de1a15a13
Colin Jaffe <balloonasaurus@gmail.com>
2019-06-14 15:40:09 -0400

    fixes command name inconsistency

--- extension.js
+++ extension.js
@@ -12,17 +12,17 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hello-world" is now active!');
+	console.log('Kerouac now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('extension.kerouacWritingMode', function () {
 		// The code you place here will be executed every time your command is executed
 		const previousSelection = {line: 0, character: 0};
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello Selection!');
+		vscode.window.showInformationMessage('Writing mode ACTIVATED.');
 		vscode.window.onDidChangeTextEditorSelection(() => {
 			// const {start, end} = event.selections[0];
 			// const characterSpan = end.character = start.character;
