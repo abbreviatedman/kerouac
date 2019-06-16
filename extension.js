0837f151f8fdd6f95708e04a275a6373eef767d3
Colin Jaffe <balloonasaurus@gmail.com>
2019-06-16 13:16:40 -0400

    moves event listener into own function

--- extension.js
+++ extension.js
@@ -1,49 +1,51 @@
 const {commands, window, Selection} = require('vscode');
 
 
+let writingMode = false;
+
 const activate = ({subscriptions}) => {
-	let writingMode = false;
-	window.onDidChangeTextEditorSelection(() => {
-		if (writingMode === true) {
-			window.activeTextEditor.selections = window
-				.activeTextEditor
-				.selections
-				.map(selection => new Selection(
-					selection.start,
-					selection.start
-					)
-				);
-		}
-	});
+  window.onDidChangeTextEditorSelection(handleSelection);
 
-	subscriptions.push(commands.registerCommand(
-		'extension.kerouac.writingMode',
-		() => {
-			if(writingMode === false) {
-				writingMode = true;
-				window.showInformationMessage('Writing mode ACTIVATED.');
-			}
-		}
-	));
+  subscriptions.push(commands.registerCommand(
+    'extension.kerouac.writingMode',
+    () => {
+      if(writingMode === false) {
+        writingMode = true;
+        window.showInformationMessage('Writing mode ACTIVATED.');
+      }
+    }
+  ));
 
-	subscriptions.push(commands.registerCommand(
-		'extension.kerouac.editingMode',
-		() => {
-			if(writingMode === true) {
-				writingMode = false;
-				window.showInformationMessage('Editing mode ACTIVATED.');
-			}
-		}
-	));
+  subscriptions.push(commands.registerCommand(
+    'extension.kerouac.editingMode',
+    () => {
+      if(writingMode === true) {
+        writingMode = false;
+        window.showInformationMessage('Editing mode ACTIVATED.');
+      }
+    }
+  ));
+}
+
+function handleSelection() {
+	if (writingMode === true) {
+		window.activeTextEditor.selections = window
+			.activeTextEditor
+			.selections
+			.map(selection => new Selection(
+				selection.start,
+				selection.start
+			));
+	}
 }
 
 exports.activate = activate;
 
 const deactivate = () => {
-	// May need this at some point.
+  // May need this at some point.
 }
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate
 }
